e7ab102591904cbfe6667e81117e57da3327092b
Fil8 <filippo.maccagni@gmail.com>
2019-12-11 12:59:52 +0100

    like it

--- gufo.py
+++ gufo.py
@@ -69,7 +69,7 @@ class gufo:
         self.cfg_par['general']['momDir'] = momDir
 
 
-        modNameDir = cfg_par[key]['runNameDir']+'myMods/'
+        modNameDir = self.cfg_par[key]['runNameDir']+'myMods/'
         cfg_par[key]['modNameDir'] = modNameDir
         if not os.path.exists(modNameDir):
             os.mkdir(modNameDir)
