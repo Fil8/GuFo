43944e25f664e7ca983b96a7d1e07cea9046d343
Fil8 <filippo.maccagni@gmail.com>
2020-01-29 12:12:51 +0100

    moments

--- scavengers/momPlay.py
+++ scavengers/momPlay.py
@@ -113,7 +113,7 @@ class momplay:
 
                 for index in match_bin:
                     thresHold = lines['g1_Amp_'+lineName][i]/noise[0,int(tabGen['PixY'][index]),int(tabGen['PixX'][index])]
-                    #print(lines['g1_Amp_'+lineName][i],noise[0,int(tabGen['PixY'][index]),int(tabGen['PixX'][index])])
+                    print(lines['g1_Amp_'+lineName][i],noise[0,int(tabGen['PixY'][index]),int(tabGen['PixX'][index])])
                     if thresHold >= lineThresh:
                         mom0G1[int(tabGen['PixY'][index]),int(tabGen['PixX'][index])] = lines['g1_Amp_'+lineName][i]
 
