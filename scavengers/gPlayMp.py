4c7781d3973e23946350dd8e5e20086b23da5db1
Fil8 <filippo.maccagni@gmail.com>
2020-02-21 14:54:42 +0100

    aaa

--- scavengers/gPlayMp.py
+++ scavengers/gPlayMp.py
@@ -486,7 +486,7 @@ def main(cfg_par):
         #for _process in processes:
         #    _process.join()
 
-        pool = mp.Pool(processes=nprocs)
+        pool = mp.Pool(4)
         #pathos = pp.ProcessPool(nprocs)
         
         #print(_process[0])
