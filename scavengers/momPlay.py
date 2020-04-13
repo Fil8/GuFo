0ff44c651465e0deb26a91e37a4f1d7920f54900
Fil8 <filippo.maccagni@gmail.com>
2020-04-13 14:39:54 +0200

    moments sn threshold from residuals

--- scavengers/momPlay.py
+++ scavengers/momPlay.py
@@ -284,7 +284,7 @@ class momplay:
                 
                 if modName=='g1':
                     thresHold = residuals['SN_OIII5006'][i]
-                    sigmaThresh = lines['g1_SigmaIntr_OIII5006'][i]
+                    sigmaThresh = lines['g1_SigIntr_OIII5006'][i]
                     ampSpax[index] = lines['g1_Amp_'+lineName][i]/tabGen['NSPAX'][index]                   
                 elif modName=='g2':
                     thresHold = (lines['g1_Amp_Hb4861'][i]+lines['g2_Amp_Hb4861'][i])/tabGen['NSPAX'][index]
