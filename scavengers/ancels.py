047d3ef9d5a6d37696534f23051eefcb82e862f3
Fil8 <filippo.maccagni@gmail.com>
2020-04-13 16:26:53 +0200

    moments sn threshold from residuals

--- scavengers/ancels.py
+++ scavengers/ancels.py
@@ -80,10 +80,10 @@ def widthCentroid(cfg_par,lines,wave,lineInfo,dLambda,sigmaCen,counter,binID,tab
         
         centreFit=result.params['g1ln'+str(ii)+'_center']
         index = np.where(tabGen['BIN_ID']==binID)[0]
-        ampSpax = tabGen[modName+'-AmpSpax_'+lineNameID][index[0]]
+        thresHold = residuals['SN_OIII5006'][index]
         indexCentroid=np.where(lines['BIN_ID']==binID)[0]
 
-        if ampSpax >= lineThresh and amp!=0.0:
+        if thresHold >= lineThresh and amp!=0.0:
             tck = interpolate.splrep(wave[:-1], lineFit, s=0)
             waveNew = np.linspace(np.min(wave),np.max(wave),1e5)
             lineFit = interpolate.splev(waveNew, tck, der=0)
