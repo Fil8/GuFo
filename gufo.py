df6281e6bf6a6ec34c627148f3d5abaec4b2491e
Fil8 <filippo.maccagni@gmail.com>
2020-06-08 17:07:13 +0200

    debug stars

--- gufo.py
+++ gufo.py
@@ -126,7 +126,7 @@ class gufo(object):
             self.cfg_par['general']['outPixSpectra'] = self.cfg_par['general']['tableDir']+'GuFo_LinePixSpectra.fits'
             self.cfg_par['general']['outVorSpectra'] = self.cfg_par['general']['outPixSpectra']
             self.cfg_par['general']['outVorLineTableName'] =self.cfg_par['general']['outVorTableName']
-            self.cfg_par['general']['outVorLines'] = cubeDir+self.cfg_par['general']['outCube']
+            self.cfg_par['general']['outVorLines'] = cubeDir+self.cfg_par['general']['outLines']
 
         outTableName = self.cfg_par['general']['runNameDir']+'gPlayOut.fits'
 
