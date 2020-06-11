6891dc572a5886cf452ae877ad528e2de6b3a85e
Fil8 <filippo.maccagni@gmail.com>
2020-06-11 16:56:42 +0200

    debug stars

--- scavengers/gPlayMp.py
+++ scavengers/gPlayMp.py
@@ -154,7 +154,7 @@ def gFitMp(cfg_par,lineInfo,vorBinInfo,wave,dd,noiseBin,counter,ii,ubins,binArr,
             counter+=1
             return counter,binArr,fitResArr,lineArr
 
-        print(binIDName)
+        print(noiseBin.shape)
 
         noiseVec = noiseBin[binIDName][:]
         # FIT
