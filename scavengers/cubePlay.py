cd0c007527b7bc86ef964c2c70e9fc4b98f66728
Fil8 <filippo.maccagni@gmail.com>
2020-07-27 16:46:00 +0200

    aa

--- scavengers/cubePlay.py
+++ scavengers/cubePlay.py
@@ -190,6 +190,7 @@ class cubeplay:
                         elif modName =='g2':
                             fit = comps['g1ln'+str(5)+'_']+comps['g2ln'+str(5)+'_']
 
+                        print(len(fit))
                         fitCube[idxMin:idxMax,int(tabGen['PixY'][index]),int(tabGen['PixX'][index])] = fit[idxMin:idxMax]
 
 
